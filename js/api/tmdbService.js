a9163a46bd246fe7242f361ab62b6d8d4601ae32
ebarsula <ebarsulai@gmail.com>
2025-06-24 02:38:14 +0300

    chore(api): add TMDB API key and base URLs as global constants for movie data integration

---
+++ js/api/tmdbService.js
@@ -0,0 +1,5 @@
+      // Global variables
+        const TMDB_API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNThlZGJhZjYzZDUzZTRhZDdiNTYyMzdiMDVkNTc3NiIsIm5iZiI6MTY5NDU2MzgzNy4zMjksInN1YiI6IjY1MDBmZGZkZTBjYTdmMDEwZGVhMzM1YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Esag7LmeXSueRKfG6Q8jFvjK0sDgK9jy_ruGaZOVgw8';
+        const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+        const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+        const BACKDROP_BASE_URL = 'https://image.tmdb.org/t/p/w1280';
