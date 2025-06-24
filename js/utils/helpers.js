ed840ec51fb1db268cffc03a9019bfe22a9a1c42
ebarsula <ebarsulai@gmail.com>
2025-06-24 04:41:47 +0300

    feat(utils): add showLoading utility to toggle loading overlay visibility

---
+++ js/utils/helpers.js
@@ -0,0 +1,10 @@
+// Utility functions
+function showLoading(show) {
+    const overlay = document.getElementById('loadingOverlay');
+    if (show) {
+        overlay.classList.remove('hidden');
+    } else {
+        overlay.classList.add('hidden');
+    }
+}
+
